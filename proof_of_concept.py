235b2c65ffa754774f383eb2d3ffa2f4f0776abf
unknown <nlow1@student.unimelb.edu.au>
2023-07-21 12:07:54 +1000

    Buggy version

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -11,20 +11,20 @@ df: pd.DataFrame = pd.read_csv('pulsars.csv')
 df['RA (deg)']  = 0
 df['Dec (deg)'] = 0
 
-# Loop over dataframe and convert RA and Dec to degrees
-for index, row in df.iterrows():
-    ra_hms  = row['RA (HMS)']
-    dec_hms = row['Dec (DMS)']
+# # Loop over dataframe and convert RA and Dec to degrees
+# for index, row in df.iterrows():
+#     ra_hms  = row['RA (HMS)']
+#     dec_hms = row['Dec (DMS)']
 
-    # Create a SkyCoord object and specify the units
-    c = SkyCoord(ra=ra_hms, dec=dec_hms, unit=(u.hourangle, u.deg))
-    # Access the converted RA and Dec in degrees
-    ra_deg  = c.ra.deg
-    dec_deg = c.dec.deg
+#     # Create a SkyCoord object and specify the units
+#     c = SkyCoord(ra=ra_hms, dec=dec_hms, unit=(u.hourangle, u.deg))
+#     # Access the converted RA and Dec in degrees
+#     ra_deg  = c.ra.deg
+#     dec_deg = c.dec.deg
 
-    # Update the DataFrame with the converted values
-    df.at[index, 'RA (deg)']  = ra_deg
-    df.at[index, 'Dec (deg)'] = dec_deg
+#     # Update the DataFrame with the converted values
+#     df.at[index, 'RA (deg)']  = ra_deg
+#     df.at[index, 'Dec (deg)'] = dec_deg
 
 # Print the DataFrame to check the degree calculation
 print(df)
